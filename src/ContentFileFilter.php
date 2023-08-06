c146f7d3eb3eee56f9232f13a5a6e88d811f60bd
Stuart Jones <stuart@savvywombat.com.au>
2023-08-06 11:09:23 +0930

    - content file filter can use an allow/block list from config to override default filtering

--- src/ContentFileFilter.php
+++ src/ContentFileFilter.php
@@ -6,9 +6,17 @@ class ContentFileFilter extends \RecursiveFilterIterator
 {
     public function accept(): bool
     {
+        $allow = [];
+        $block = [];
+        if (! empty($_ENV['CONFIG']['files'])) {
+            $allow = $_ENV['CONFIG']['files']['allow'] ?? [];
+            $block = $_ENV['CONFIG']['files']['block'] ?? [];
+        }
+
+        $pathname = str_replace($_ENV['WORKING_DIR'] . '/', '', $this->current()->getPathname());
         $filename = $this->current()->getFilename();
 
-        if ($filename[0] === '.' || $filename[0] === '_') {
+        if (in_array($pathname, $block) || (! in_array($pathname, $allow) && ($filename[0] === '.' || $filename[0] === '_'))) {
             // skip hidden files and directories
             return false;
         }
