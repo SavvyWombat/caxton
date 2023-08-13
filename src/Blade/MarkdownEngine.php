41c0f3018544ed97ba5d67b31dae018355addbd3
Stuart Jones <stuart@savvywombat.com.au>
2023-08-13 10:49:37 +0930

    - separate front matter from content in markdown files

--- src/Blade/MarkdownEngine.php
+++ src/Blade/MarkdownEngine.php
@@ -10,6 +10,11 @@ use League\CommonMark\Exception\CommonMarkException;
 
 class MarkdownEngine implements Engine
 {
+    const EXTRACTION_PATTERN = "#^\s*---"
+    . "[\r\n|\n]*(.*?)[\r\n|\n]+" // front end matter [1]
+    . "---[\r\n|\n]*"
+    . "(.*)$#s"; // content [2]
+
     public function __construct(
         protected CompilerEngine $blade,
         protected CommonMarkConverter $markdown,
@@ -27,7 +32,25 @@ class MarkdownEngine implements Engine
     public function get($path, array $data = []): string
     {
         return $this->markdown->convert(
-            $this->blade->get($path, $data)
+            $this->extractContent($this->blade->get($path, $data))
         )->getContent();
+    }
+
+    public function extractFrontMatter($content)
+    {
+        if (preg_match(self::EXTRACTION_PATTERN, $content, $matches)) {
+            return $matches[1];
+        }
+
+        return $content;
+    }
+
+    public function extractContent($content)
+    {
+        if (preg_match(self::EXTRACTION_PATTERN, $content, $matches)) {
+            return $matches[2];
+        }
+
+        return $content;
     }
 }
